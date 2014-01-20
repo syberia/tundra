abb60f880284056e9cb4d4945fa469e53e18ceb0
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-01-20 15:14:01 -0600

    remove verboses and fix predict value

--- inst/tests/test-tundra_ensemble.r
+++ inst/tests/test-tundra_ensemble.r
@@ -5,9 +5,9 @@ assign('tundra_simple', function(mp = list(), defaults = list()) {
   tundra_container$new('simple',
     function(dataframe) output$val <<- dataframe[1, 1] + input$twiddle,
     function(dataframe)
-      if (input$master) apply(dataframe, 1, sum)
+      if (input$master) output$val + apply(dataframe, 1, sum)
       else rep(output$val, nrow(dataframe)),
-    mp, append(defaults, list(master = FALSE)))
+    mp, append(defaults, list(master = FALSE, twiddle = 0)))
 }, envir = globalenv())
 
 test_that('the simple tundra model works correctly', {
@@ -19,13 +19,15 @@ test_that('the simple tundra model works correctly', {
 
 test_that('it correctly trains a trivial ensemble', {
   twiddle <- runif(10, 0, 1)
+  num_buckets <- 10
   test_ensemble <- tundra_ensemble(list(), list(
-    validation_buckets = 10,
+    validation_buckets = num_buckets,
     master = list('simple', master = TRUE),
     submodels = lapply(twiddle, function(tw) list('simple', twiddle = tw))
   ))
-  test_ensemble$train(iris, verbose = TRUE)
-  expect_equal(test_ensemble$predict(iris[1,], verbose = TRUE),
+  test_ensemble$train(iris)
+  expect_equal(test_ensemble$predict(iris[1,]),
+               iris[1 + floor(nrow(iris) / num_buckets), 1] + twiddle[1] +
                iris[1, 1] * length(twiddle) + sum(twiddle))
 })
 
