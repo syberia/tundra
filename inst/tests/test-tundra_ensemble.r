1d55f5ddd83cb22ae95c620607da842dad867fcb
Robert Krzyzanowski <technoguyrob@gmail.com>
2014-01-20 15:17:49 -0600

    move tundra_simple into helper file

--- inst/tests/test-tundra_ensemble.r
+++ inst/tests/test-tundra_ensemble.r
@@ -1,14 +1,6 @@
 context('tundra ensemble')
 
-# A very simple model mock for testing tundra containers
-assign('tundra_simple', function(mp = list(), defaults = list()) {
-  tundra_container$new('simple',
-    function(dataframe) output$val <<- dataframe[1, 1] + input$twiddle,
-    function(dataframe)
-      if (input$master) output$val + apply(dataframe, 1, sum)
-      else rep(output$val, nrow(dataframe)),
-    mp, append(defaults, list(master = FALSE, twiddle = 0)))
-}, envir = globalenv())
+assign('tundra_simple', source('tundra_simple.r')$value, envir = globalenv())
 
 test_that('the simple tundra model works correctly', {
   simple_model <- tundra_simple(, list(twiddle = 0))
