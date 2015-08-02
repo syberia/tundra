b3b73883aba40ca5a5159eb16543ab506c1a6bc6
RobertZK <technoguyrob@gmail.com>
2015-08-02 11:56:18 -0500

    Test permutation of call_with possibilities.

---
+++ tests/testthat/test-call_with.R
@@ -0,0 +1,24 @@
+context("call_with")
+
+test_that("it correctly injects a simple example", {
+  fn <- local({ x <- 1; function(y) { x + y } })
+  expect_identical(call_with(fn, list(1), list(x = 2)), 3)
+})
+
+test_that("it correctly injects a simple example with an environment", {
+  fn <- local({ x <- 1; function(y) { x + y } })
+  expect_identical(call_with(fn, list(1), list2env(list(x = 2))), 3)
+})
+
+test_that("it correctly injects a simple example with an environment override", {
+  y <- 2
+  fn <- local({ x <- 1; y <- 1; function(z) { c(x, y, z) } })
+  expect_identical(call_with(fn, list(1), list2env(list(x = 2))), c(2, 2, 1))
+})
+
+test_that("it correctly injects a simple example with a list", {
+  y <- 2
+  fn <- local({ x <- 1; y <- 1; function(z) { c(x, y, z) } })
+  expect_identical(call_with(fn, list(1), list(x = 2)), c(2, 1, 1))
+})
+
