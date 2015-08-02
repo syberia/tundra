2c47f2fa196fc164952a6d38d2e3b3b18cd31c04
RobertZK <technoguyrob@gmail.com>
2015-08-02 10:52:32 -0500

    Initial members of a tundraContainer object.

--- R/tundraContainer.R
+++ R/tundraContainer.R
@@ -5,6 +5,17 @@
 #' @export
 tundraContainer <- R6::R6Class("tundraContainer",
   public = list(
+    .keyword          = NULL, # character
+    .train_function   = NULL, # function or NULL
+    .predict_function = NULL, # function or NULL
+    .munge_procedure  = NULL, # list of mungepieces
+    .default_args     = NULL, # list
+    .trained          = FALSE, # logical
+    .input            = NULL, # environment
+    .output           = NULL, # environment
+    .internal         = NULL, # environment
+    .hooks            = NULL, # list
+
     initialize = function(...) {
     }
   )
