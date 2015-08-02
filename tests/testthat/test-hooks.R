8a175b0fec0fd630137cf018f12625e1fa154f24
RobertZK <technoguyrob@gmail.com>
2015-08-02 14:23:30 -0500

    Test that train_finalize hook is in the correct order.

--- tests/testthat/test-hooks.R
+++ tests/testthat/test-hooks.R
@@ -10,6 +10,10 @@ let(container_with_munge_side_effect, function(env) {
   tundraContainer$new("foo", munge_procedure = list(mp))
 })
 
+let(container_with_train_side_effect, function(env) {
+  tundraContainer$new("foo", function(data) { env$effect <- c(env$effect, "train") })
+})
+
 describe("Invalid inputs", {
   test_that("it errors if an invalid hook name is provided", {
     expect_error(container$add_hook("foo"))
@@ -49,11 +53,11 @@ test_that("it runs the train post-munge hook in the correct order", {
 })
 
 test_that("it runs the train_finalize hook in the correct order", {
+  container <- container_with_train_side_effect(env)
   env  <- list2env(list(effect = character(0)))
   hook <- function() { env$effect <- c(env$effect, "hook") }
-  container <- container_with_munge_side_effect(env)
-  container$add_hook("train_post_munge", hook)
+  container$add_hook("train_finalize", hook)
   container$train(iris)
-  expect_equal(env$effect, c("mungebit", "hook"))
+  expect_equal(env$effect, c("train", "hook"))
 })
 
