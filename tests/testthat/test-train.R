091a6c4b7a6e9aca24bce094aa79ee67e6fafbd5
RobertZK <technoguyrob@gmail.com>
2015-08-02 13:57:08 -0500

    Test that a tundraContainer cannot be trained twice.

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -1,5 +1,13 @@
 context("tundraContainer$train")
 
+describe("Invalid inputs", {
+  test_that("it cannot be trained twice", {
+    container <- tundraContainer$new("foo")
+    container$train(iris)
+    expect_error(container$train(iris), "has already been trained")
+  })
+})
+
 test_that("it can train a simple example", {
   container <- tundraContainer$new("foo", function(data) {
     output$data <- data
